6dd8ca1b996916427df1b5659b2d7cc362cf92fc
Aaron De Leon <dl.aaron25@gmail.com>
2015-09-15 23:24:27 +0800

    account model
    - enryption and decryption function implemented

--- application/models/Account.php
+++ application/models/Account.php
@@ -9,32 +9,31 @@ class Account extends CI_Model{
 
 	function register(){
 		$data = $_POST;
-		$status = FALSE;
+		$user['username'] = $data['username'];
 		if(isset($data['username']) && isset($data['password'])){
 			unset($_POST);
-			$query = $this->db->get_where('account',$data);
+			$query = $this->db->get_where('account',$user);
 			if(!$query->num_rows()){
 				$status = TRUE;
 				$this->db->insert('account',$data);
 			}
 		}
-		unset($data);
 		return $status;
 	}
 
 	function login(){
 		$data = $_POST;
 		$status = FALSE;
+		$user['username'] = $data['username'];
 		if(isset($data['username']) && isset($data['password'])){
 			unset($_POST);
-			$query = $this->db->get_where('account',$data);
-			if($query->num_rows()){
+			$query = $this->db->get_where('account',$user);
+			if($query->num_rows() && ($this->encrypt->decode($query->row()->password) == $data['password']) ){
 				$status = TRUE;
 				$_SESSION['username'] = $query->row()->username;
 				$_SESSION['is_login'] = TRUE;
 			}
 		}
-		unset($data);
 		return $status;
 	}
 }
