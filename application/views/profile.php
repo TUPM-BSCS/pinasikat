314b4b16618e4d3c19e2adf574eb8252ffb98948
nicosolitana <nicosolitana@gmail.com>
2015-10-17 02:33:23 +0800

    Updated by Nico

---
+++ application/views/profile.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <title>PINASikat</title>
+    <meta charset="utf-8">
+  	<link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <style> body{ font-family: Segoe UI; }</style>
+  </head>
+
+    <body>
+    	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    	<script type="text/javascript" src="js/materialize.min.js"></script>
+
+      <div class="row center blue">
+        <img src="<?php echo base_url();?>images/user.png" class="circle responsive-img">
+      </div>
+
+      <div class="container">
+        <div class="row left-align ">
+          <div class="container">
+            <h3 class="blue-text"> About </h3>
+
+            <p><h5 class="blue-text" >Maria Lorem Ipsum dela Lorem Ipsum</h5><h6 class="blue-text" >NAME</h6><p>
+            <p><h5 class="blue-text" >Block 3 Lot 3, Lorem Ipsum, Lorem City, Ipsum Province </h5><h6 class="blue-text" >ADDRESS</h6><p>
+            <p><h5 class="blue-text" >Lorem Ipsum</h5><h6 class="blue-text" >PROFESSION</h6><p>
+            <p><h5 class="blue-text" >Lorem Ipsum University of the Philippines</h5><h6 class="blue-text" >EDUCATION</h6><p>
+            <p><h5 class="blue-text" >0914 768 2345</h5><h6 class="blue-text" >CONTACT INFORMATION</h6><p>
+            <p><h5 class="blue-text" >LoremIpsum@outlook.ph</h5><h6 class="blue-text" >EMAIL ADDRESS</h6><p>
+          </div>
+        </div>
+      </div>
+    </body>
+</html>
