715ef41851d4d0b387b07450356dbf19558b7131
samallorde <sameeraalim@ymail.com>
2015-10-17 20:59:40 +0800

    uploadform changes

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -11,7 +11,7 @@
           </div>
         </div>
         <div class="row">
-          <div class="input-field col s12">
+          <div class="input-field col s10">
             <textarea id="textarea1" class="materialize-textarea" length="120"></textarea>
             <label for="textarea1">Description</label>
           </div>
@@ -21,26 +21,80 @@
         </div>
       </form>
 
+    </div><main>
+
+<div class="content">
+
+  <div class="row">
+      <form class="col s11">
+        <div class="row">
+          <div class="input-field col s8">
+            <input id="input_text" type="text" length="10">
+            <label for="input_text">Article Name</label>
+          </div>
+        </div>
+        <div class="row">
+           <div class="input-field col s11">
+                  <textarea id="textarea1" class="materialize-textarea"></textarea>
+                  <label for="textarea1">Description</label>
+                  <button class="btn waves-effect waves-light right" type="submit" name="action">Edit</button>
+            </div>
+        </div>
+      </form>
+
     </div>
 
    
         
   <div class="row">  
-  <form action="#">
-      <div class="file-field input-field">
-          <a class="btn-floating btn-large waves-effect waves-light">
+    <form action="#">
+        <div class="file-field input-field">
+            <a class="btn-floating btn-large waves-effect waves-light">
+            <input type="file" multiple>
+            <i class="material-icons">add</i>
+          </a>
+          <div class="file-path-wrapper col s7">
+            <input class="file-path validate" type="text" placeholder="Upload one or more images">
+          </div>
+        </div>
+
+        <div class="row">
+        <form class="col s11">
+            <div class="row">
+              <div class="input-field col s10">
+                <textarea id="textarea1" class="materialize-textarea"></textarea>
+                 <label for="textarea1">Caption</label>
+                 <button class="btn waves-effect waves-light right" type="submit" name="action">Add</button>
+              </div>
+            </div>  
+        </form>
+        </div>
+
+    </form> 
+   </div>
+     
+        
+</div>
+</main>
+
+   
+        
+  <div class="row">  
+  	<form action="#">
+      	<div class="file-field input-field">
+            <a class="btn-floating btn-large waves-effect waves-light">
             <input type="file" multiple>
             <i class="material-icons">add</i>
           </a>
           <div class="file-path-wrapper">
             <input class="file-path validate" type="text" placeholder="Upload one or more images">
           </div>
-      </div>
+      	</div>
 
-      <div class="row">
-        <form class="col s12">
+      	<div class="row">
+        <form class="col s10">
             <div class="row">
-              <div class="input-field col s12">
+              <div class="input-field col s10">
                   <textarea id="textarea1" class="materialize-textarea"></textarea>
                   <label for="textarea1">Caption</label>
               </div>
@@ -49,10 +103,10 @@
               <button class="btn waves-effect waves-light right" type="submit" name="action">Add</button>
             </div>
         </form>
+      	</div>
 
-      </div>
-    </form> 
-    </div>
+   	</form> 
+   </div>
      
         
 </div>
