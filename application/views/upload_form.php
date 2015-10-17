eebc6afbfdcf73b11321c0daca9a5e4c744514d6
samallorde <sameeraalim@ymail.com>
2015-10-17 19:57:58 +0800

    Upload form ui
    check and pasabi na lang dapat icorrect. Thanksss

--- application/views/upload_form.php
+++ application/views/upload_form.php
@@ -1,20 +1,59 @@
-<html>
-<head>
-<title>Upload Form</title>
-</head>
-<body>
+<main>
 
-<?php echo $error;?>
+<div class="content">
 
-<?php echo form_open_multipart('upload');?>
+  <div class="row">
+      <form class="col s12">
+        <div class="row">
+          <div class="input-field col s6">
+            <input id="input_text" type="text" length="10">
+            <label for="input_text">Article Name</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <textarea id="textarea1" class="materialize-textarea" length="120"></textarea>
+            <label for="textarea1">Description</label>
+          </div>
+        </div>
+        <div class="row">
+          <button class="btn waves-effect waves-light right" type="submit" name="action">Edit</button> 
+        </div>
+      </form>
 
-<input type="file" name="userfile" size="20" />
+    </div>
 
-<br /><br />
+   
+        
+  <div class="row">  
+  <form action="#">
+      <div class="file-field input-field">
+          <a class="btn-floating btn-large waves-effect waves-light">
+            <input type="file" multiple>
+            <i class="material-icons">add</i>
+          </a>
+          <div class="file-path-wrapper">
+            <input class="file-path validate" type="text" placeholder="Upload one or more images">
+          </div>
+      </div>
 
-<input type="submit" value="upload" />
+      <div class="row">
+        <form class="col s12">
+            <div class="row">
+              <div class="input-field col s12">
+                  <textarea id="textarea1" class="materialize-textarea"></textarea>
+                  <label for="textarea1">Caption</label>
+              </div>
+            </div>  
+            <div class="row">  
+              <button class="btn waves-effect waves-light right" type="submit" name="action">Add</button>
+            </div>
+        </form>
 
-</form>
-
-</body>
-</html>
+      </div>
+    </form> 
+    </div>
+     
+        
+</div>
+</main>
