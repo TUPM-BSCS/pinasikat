3013bc22227ca19035e62039dffcc2f627387f21
nicosolitana <nicosolitana@gmail.com>
2015-10-21 04:18:38 +0800

    [PROFILE] Graphical User Interface Revised

--- application/views/profile.php
+++ application/views/profile.php
@@ -1,19 +1,39 @@
 
 <main>
   <div class="row">
-    <div class="col s6">
-      <img src="<?php echo base_url();?>images/user.png" class="circle responsive-img">
+    <div class="col s12 center black"> <br />
+      <img src="<?php echo base_url();?>images/user.png" class="circle responsive-img"> <br /><br />
+    </div>
+    <div class="col s12 white">
+      <h4 class="right"> User's Personal Information </h4>
     </div>
   </div>
   <div class="row left-align ">
     <div class="col s12">
-      <h3 class="blue-text"> About </h3>
-      <p><h5 class="blue-text" >Maria Lorem Ipsum dela Lorem Ipsum</h5><h6 class="blue-text" >NAME</h6><p>
-      <p><h5 class="blue-text" >Block 3 Lot 3, Lorem Ipsum, Lorem City, Ipsum Province </h5><h6 class="blue-text" >ADDRESS</h6><p>
-      <p><h5 class="blue-text" >Lorem Ipsum</h5><h6 class="blue-text" >PROFESSION</h6><p>
-      <p><h5 class="blue-text" >Lorem Ipsum University of the Philippines</h5><h6 class="blue-text" >EDUCATION</h6><p>
-      <p><h5 class="blue-text" >0914 768 2345</h5><h6 class="blue-text" >CONTACT INFORMATION</h6><p>
-      <p><h5 class="blue-text" >LoremIpsum@outlook.ph</h5><h6 class="blue-text" >EMAIL ADDRESS</h6><p>
+      <div class="row">
+        <div class="col s2"><img src="<?php echo base_url();?>images/usersmall.png" class="circle right responsive-img"> </div>
+        <div class="col s10"><p class="left"><h5 class="white-text">Maria Lorem Ipsum dela Lorem Ipsum</h5><h6 class="white-text">NAME</h6><p></div>
+      </div>
+      <div class="row">
+        <div class="col s2"><img src="<?php echo base_url();?>images/house.png" class="circle right responsive-img"> </div>
+        <div class="col s10"> <p><h5 class="white-text">Block 3 Lot 3, Lorem Ipsum, Lorem City, Ipsum Province </h5><h6 class="white-text">ADDRESS</h6><p></div>
+      </div>
+      <div class="row">
+        <div class="col s2"><img src="<?php echo base_url();?>images/light.png" class="circle right responsive-img"> </div>
+        <div class="col s10"> <p><h5 class="white-text">Lorem Ipsum</h5><h6 class="white-text">PROFESSION</h6><p></div>
+      </div>
+      <div class="row">
+        <div class="col s2"><img src="<?php echo base_url();?>images/grad.png" class="circle right responsive-img"> </div>
+        <div class="col s10"> <p><h5 class="white-text">Lorem Ipsum University of the Philippines</h5><h6 class="white-text">EDUCATION</h6><p></div>
+      </div>
+      <div class="row">
+        <div class="col s2"><img src="<?php echo base_url();?>images/teleph.png" class="circle right responsive-img"> </div>
+        <div class="col s10"> <p><h5 class="white-text">0914 768 2345</h5><h6 class="white-text">CONTACT INFORMATION</h6><p></div>
+      </div>
+      <div class="row">
+        <div class="col s2"><img src="<?php echo base_url();?>images/email.png" class="circle right responsive-img"> </div>
+        <div class="col s10"> <p><h5 class="white-text">LoremIpsum@outlook.ph</h5><h6 class="white-text">EMAIL ADDRESS</h6><p> </div>
+      </div>
     </div>
   </div>
 </main>
